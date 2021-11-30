3e74878f24e94e187055df207d095f43ed9aeb15
liptaki13 <95048497+liptaki13@users.noreply.github.com>
2021-11-29 21:25:00 -0500

    Added an Input Form that is Tied to Local State
    Added a input form that sets Local Component state and displays that state in the pokedex header.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import PokedexHeader from './components/PokedexHeader'
+import {
+	Input,
+	Segment
+} from'semantic-ui-react'
 import './App.css';
 
 class App extends React.Component {
-	constructor(props){
+	constructor(props) {
 		 super(props)
 		 this.state = {
-		 	 name: "Ivan"
+		 		author: "@liptaki13",
+		 	  name: "Ivan"
 		 }
+		 this.onChange = this.onChange.bind(this)
+	}
+
+	onChange(event, data) {
+		this.setState({
+			name: data.value
+		})
 	}
 
   render() {
   	return(
       <div className="App">
         <PokedexHeader 
-        	author='@liptaki13' 
+        	author={this.state.author} 
         	name={this.state.name} 
-        /> 
+        />
+        <Segment basic>
+        	<Input 
+        		label={"Your Name"}
+        		onChange={this.onChange}
+        		value={this.state.name}
+        	/>
+
+        </Segment>
       </div>
     );
   }
